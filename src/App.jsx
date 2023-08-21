22bca846da456356a0528f1c05bfa72317492f0c
sergioedo <sergio.edo@gmail.com>
2023-08-21 18:16:55 +0200

    characters selector - total value state

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,23 @@
+import { useState } from "react";
 import "./App.css";
 
 const App = () => {
+  const [totalValue, setTotalValue] = useState(13.5);
   return (
     <>
       <h1>Calculadora de precios de muñecos cabezones</h1>
 
       <div>
         <label>Elige un personaje:</label>
-        <select id="character">
+        <select onChange={(e) => setTotalValue(Number(e.target.value))}>
           <option value="13.5">Jon Snow (13.5€)</option>
           <option value="16.5">Danerys Targaryen (16.5€)</option>
           <option value="19">Tyrion Lannister (19€)</option>
         </select>
       </div>
 
-      <div id="result">
-        Precio total: <span id="total">13.50€</span>
+      <div>
+        Precio total: <span>{totalValue.toFixed(2)}€</span>
       </div>
     </>
   );
